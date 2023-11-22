1f8bb8e6b1b29beeb0ee3461178af7dfd78cec59
mdpfeifer1 <mikelaidenpfeifer@gmail.com>
2023-11-21 20:24:44 -0600

    finished TODOs

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,32 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // prevent default behavior to avoid auto prompt
+    event.preventDefault();
+    // Store the event for later use
+    deferredPrompt = event;
+    // Show install button
+    butInstall.style.display = 'block'
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+        const choiceResult = await deferredPrompt.userChoice;
+
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted PWA installation');
+        } else {
+            console.log('User dismissed the PWA installation');
+        }
+        deferredPrompt = null;
+        butInstall.style.display = 'none';
+    }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('App installed successfully');
+});
